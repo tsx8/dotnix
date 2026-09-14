41491fc8be53e3d486b8b795649791931c0b3d4f
tsx8 <tangsongxiaoba@163.com>
2026-09-14 22:59:12 +0800

    Configure PingFang fonts and update inputs
    Require the original PingFang font file locally and use it for
    system and KDE font defaults.

--- modules/personal/desktop/session.nix
+++ modules/personal/desktop/session.nix
@@ -11,12 +11,27 @@
 
     fonts = {
       packages = with pkgs; [
+        (runCommand "pingfang-sc"
+          {
+            src = requireFile {
+              name = "PingFang.ttc";
+              sha256 = "6bccdb1a967b2ae7e856927eb559591b2ce05656b0d4ad2764b9c9429f12c0b7";
+              message = "Run nix-store --add-fixed sha256 /path/to/PingFang.ttc with the original font file.";
+            };
+          }
+          ''
+            install -Dm644 "$src" "$out/share/fonts/truetype/PingFang.ttc"
+          ''
+        )
         noto-fonts-cjk-sans
         noto-fonts-cjk-serif
       ];
 
       fontconfig.defaultFonts = {
-        sansSerif = lib.mkAfter [ "Noto Sans CJK SC" ];
+        sansSerif = [
+          "PingFang SC"
+          "Noto Sans CJK SC"
+        ];
         serif = lib.mkAfter [ "Noto Serif CJK SC" ];
       };
     };
@@ -34,4 +49,23 @@
       pulse.enable = true;
     };
   };
+
+  dotnix.modules.home =
+    let
+      normalFont = "PingFang SC,10,-1,5,400,0,0,0,0,0";
+      smallFont = "PingFang SC,8,-1,5,400,0,0,0,0,0";
+    in
+    {
+      # 逐项写入，保留 KDE 管理的主题、颜色及其他设置。
+      qt.kde.settings.kdeglobals = {
+        General = {
+          font = normalFont;
+          menuFont = normalFont;
+          toolBarFont = normalFont;
+          taskbarFont = normalFont;
+          smallestReadableFont = smallFont;
+        };
+        WM.activeFont = normalFont;
+      };
+    };
 }
